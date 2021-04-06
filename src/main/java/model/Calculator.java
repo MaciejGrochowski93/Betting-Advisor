f2d9639da87b6eec6065a074cf81bc24fc753c0f
Maciej <maciejgrochowski93@o2.pl>
2021-04-06 23:50:24 +0200

    adding several new methods and mappings

--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -15,7 +15,7 @@ public class Calculator {
     }
 
     public double calculateProbability(double actualOdds) throws IncorrectArgument {
-        if (actualOdds > 1 && actualOdds < 1000) {
+        if (isOddsCorrect(actualOdds)) {
             result = roundTwoDigits(tmpResult = 100 / actualOdds);
         } else
             throw new IncorrectArgument();
@@ -31,11 +31,45 @@ public class Calculator {
         return result;
     }
 
-    public double reverseOdds(double bookmakerOdds) {
-        if (bookmakerOdds > 1 && bookmakerOdds < 1000) {
+    public double reverseOdds(double bookmakerOdds) throws IncorrectArgument {
+        if (isOddsCorrect(bookmakerOdds)) {
             result = roundTwoDigits((1 / (bookmakerOdds - 1)) + 1);
+        } else
+            throw new IncorrectArgument();
+        return result;
+    }
+
+    public String calculateSurebet(double money, double odds1, double odds2) {
+        double multiplier = calculateSurebetMultiplier(odds1, odds2);
+        double moneyToBetOnOdds1 = (money * multiplier) / odds1;
+        double moneyToBetOnOdds2 = (money * multiplier) / odds2;
+        return ("In order to create a " + roundTwoDigits(multiplier * 100) + "% surebet from odds: " + odds1 + ", " + odds2 + " and total money of "
+                + money + ", you should bet " + roundTwoDigits(moneyToBetOnOdds1) + " at odds " + odds1 + ", and " + roundTwoDigits(moneyToBetOnOdds2) +
+                " at odds " + odds2 + ". Your return should equal: " + roundTwoDigits(money * multiplier) + ", with the gross profit of "
+                + roundTwoDigits(money * (multiplier - 1)) + "."
+        );
+    }
+
+    public double calculateSurebetMultiplier(double odds1, double odds2) {
+        if (isOddsCorrect(odds1) && isOddsCorrect(odds2) && isSurebet(odds1, odds2)) {
+            result = (odds1 * odds2) / (odds1 + odds2);
         }
         return result;
+    }
+
+    public boolean isSurebet(double odds1, double odds2) {
+        if (isOddsCorrect(odds1) && isOddsCorrect(odds2)) {
+            if ((odds1 - 1) * (odds2 - 1) > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isOddsCorrect(double odds) {
+        if (odds > 1 && odds < 1000)
+            return true;
+        return false;
     }
 
     public double roundTwoDigits(double odds) {
